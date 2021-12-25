fca2ba10c6ec15a7f6d69a1468f67b6c09a7594f
cloud_user <prem1218@gmail.com>
2021-12-25 11:20:10 +0000

    new file

--- main.tf
+++ main.tf
@@ -1,9 +1,25 @@
 provider "aws" {
   region = "us-east-1"
 }
+resource "aws_vpc" "main" {
+  cidr_block = "10.5.0.0/16"
+
+  tags = {
+    Name = "tuts vpc"
+  }
+}
+
+resource "aws_subnet" "web" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.5.0.0/16"
+
+  tags = {
+    Name = "aws subnet"
+  }
+}
 resource "aws_instance" "vm" {
-  ami           = "ami-061ac2e015473fbe2"
-  subnet_id     = "subnet-0cdddedbf69e12f61"
+  ami           = "ami-0ed9277fb7eb570c9"
+ # subnet_id     = "subnet-0cdddedbf69e12f61"
   instance_type = "t2.micro"
   tags = {
     Name = "my-first-tf-node-prem"
