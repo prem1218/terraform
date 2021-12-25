5477e74b05e0e1a8f7b92caf70ed3046f0db742d
prem1218 <prem1218@gmail.com>
2021-12-25 13:27:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 resource "aws_instance" "vm" {
   ami           = "ami-061ac2e015473fbe2"
-  subnet_id     = "subnet-0b63682cb0cb116a5"
+  subnet_id     = "subnet-0cdddedbf69e12f61"
   instance_type = "t2.micro"
   tags = {
     Name = "my-first-tf-node_prem"
